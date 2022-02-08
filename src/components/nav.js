c2bbb10c58faa43998080b4da926b45e3578653d
mylonjones <mylonndj1234@gmail.com>
2022-02-07 18:44:54 -0800

    added styling to home page

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,11 +3,20 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return(
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/'>About</Link>
-      <Link to='/'>Contact</Link>
-      <Link to='/'>More</Link>
+    <nav className='dashPartition'>
+      <img
+        className='logo'
+        src='photos/logo.png'
+        alt='logo'
+        height='60'
+        width='60'
+      />
+      <div>
+        <Link className='navLink' to='/'>Home</Link>
+        <Link className='navLink' to='about'>About</Link>
+        <Link className='navLink' to='contact'>Contact</Link>
+        <Link className='navLink' to='more'>More</Link>
+      </div>
     </nav>
   )
 }
